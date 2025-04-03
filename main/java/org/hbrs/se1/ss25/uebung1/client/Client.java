a2824e369222da681cf0151247c96547550f1d53
Andreas Hackelöer <andreas.hackeloeer@h-brs.de>
2025-04-03 12:04:46 +0200

    Initial commit.

---
+++ main/java/org/hbrs/se1/ss25/uebung1/client/Client.java
@@ -0,0 +1,8 @@
+package org.hbrs.se1.ss25.uebung1.client;
+
+public class Client {
+    public void printTransformation(int number) {
+        String result = ""; // Hier fehlt noch was
+        System.out.println("Die Hexadezimaldarstellung der Zahl " + number + " ist: " + result);
+    }
+}
---
+++ test/java/org/hbrs/se1/ss25/uebung1/test/TestHexNumberTransformer.java
@@ -0,0 +1,11 @@
+package org.hbrs.se1.ss25.uebung1.test;
+
+import org.junit.jupiter.api.Test;
+
+class TestHexNumberTransformer {
+
+    @Test
+    void TestHexNumbers() {
+        // Testcode
+    }
+}
